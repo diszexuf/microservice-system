96942278a2a3b7d4e840852ad9efc2632dbf885c
diszexuf <diszexuf@yandex.ru>
2026-01-27 17:32:04 +0500

    feat: create person-service module

---
+++ person-service/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.10"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.github.diszexuf"
+version = "1.0.0-SNAPSHOT"
+description = "person-service"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(24)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
