7916d9748bb2e0c9136c4a9949cb5e283cae9f81
diszexuf <diszexuf@yandex.ru>
2026-02-11 23:47:15 +0500

    feat: create api gateway service. Add openapi specifications

---
+++ api/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.0"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.github.diszexuf"
+version = "1.0.0-SNAPSHOT"
+description = "API Gateway for study project"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(24)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    testImplementation("org.springframework.boot:spring-boot-starter-webflux-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
